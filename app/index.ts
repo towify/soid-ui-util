56e2343135de4010af73c36df7a6406d7b996893
allenzhang-towify <allen.zhang@towify.com>
2020-11-24 17:46:56 +0800

    feat(grid): 重构grid, 分拆成 GridChildrenService 和 GridLineService

--- app/index.ts
+++ app/index.ts
@@ -2,8 +2,11 @@
  * @author kaysaith
  * @date 2020/4/4 01:43
  */
-export * from './service/grid.area.service/grid.area.service';
-export * from './service/grid.area.service/grid.area.service.interface';
+
+export * from './service/grid.service/grid.children.service';
+export * from './service/grid.service/grid.children.service.interface';
+export * from './service/grid.service/grid.line.service';
+export * from './service/grid.service/grid.line.service.interface';
 export * from './manager/box.shadow.manager/box.shadow.manager';
 export * from './manager/box.shadow.manager/box.shadow.manager.interface';
 export * from './manager/color.manager/color.manager.interface';
