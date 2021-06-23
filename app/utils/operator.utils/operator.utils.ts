c0755609bbb92d4a172c090afe358502f12f00b0
allenzhang-towify <allen.zhang@towify.com>
2021-06-23 14:24:00 +0800

    fixed(operator): fix some bugs

--- app/utils/operator.utils/operator.utils.ts
+++ app/utils/operator.utils/operator.utils.ts
@@ -15,10 +15,7 @@ export class OperatorUtils {
     return rect.x < xRange.to && rect.x + rect.width > xRange.from;
   }
 
-  static getRectListInYRange(
-    yRange: RegionInfo,
-    rectList: RectInfo[]
-  ): RectInfo[] {
+  static getRectListInYRange(yRange: RegionInfo, rectList: RectInfo[]): RectInfo[] {
     return rectList.reduce<RectInfo[]>((previous, current) => {
       if (OperatorUtils.checkY(yRange, current)) {
         return previous.concat(current);
@@ -27,10 +24,7 @@ export class OperatorUtils {
     }, []);
   }
 
-  static getRectListInXRange(
-    xRange: RegionInfo,
-    rectList: RectInfo[]
-  ): RectInfo[] {
+  static getRectListInXRange(xRange: RegionInfo, rectList: RectInfo[]): RectInfo[] {
     return rectList.reduce<RectInfo[]>((previous, current) => {
       if (OperatorUtils.checkX(xRange, current)) {
         return previous.concat(current);
@@ -89,6 +83,7 @@ export class OperatorUtils {
     let result = 0;
     if (rect.x > parentRect.x + parentRect.width) {
       if (
+        (rect.y + rect.height > parentRect.y && rect.y < parentRect.y + parentRect.height) ||
         Math.abs(rect.y + rect.height - parentRect.y) <
           Math.abs(rect.x - (parentRect.x + parentRect.width)) ||
         Math.abs(rect.y - parentRect.y - parentRect.height) <
@@ -99,24 +94,23 @@ export class OperatorUtils {
         verticalOffset = parentRect.y - rect.y;
         horizontalOffset = rect.x - parentRect.x - parentRect.width;
       } else {
-        verticalOffset =
-          rect.y + rect.height - parentRect.y - parentRect.height;
+        verticalOffset = rect.y + rect.height - parentRect.y - parentRect.height;
         horizontalOffset = rect.x - parentRect.x - parentRect.width;
       }
-    } else if (rect.x < parentRect.x - rect.width) {
+    } else if (rect.x + rect.width < parentRect.x) {
       if (
+        (rect.y + rect.height > parentRect.y && rect.y < parentRect.y + parentRect.height) ||
         Math.abs(rect.y + rect.height - parentRect.y) <
-          Math.abs(rect.x + rect.width - parentRect.x) ||
+          Math.abs(parentRect.x - rect.x - rect.width) ||
         Math.abs(rect.y - parentRect.y - parentRect.height) <
-          Math.abs(rect.x + rect.width - parentRect.x)
+          Math.abs(parentRect.x - rect.x - rect.width)
       ) {
         result = parentRect.x - (rect.x + rect.width);
       } else if (rect.y < parentRect.y) {
         verticalOffset = parentRect.y - rect.y;
         horizontalOffset = parentRect.x - rect.x - rect.width;
       } else {
-        verticalOffset =
-          rect.y + rect.height - parentRect.y - parentRect.height;
+        verticalOffset = rect.y + rect.height - parentRect.y - parentRect.height;
         horizontalOffset = parentRect.x - rect.x - rect.width;
       }
     }
