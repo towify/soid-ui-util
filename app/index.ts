592e83b70e86da9fd2af21344802591532e7176d
allen <allen.zhang@towify.com>
2022-07-04 17:57:16 +0800

    fixed(animation): change index

--- app/index.ts
+++ app/index.ts
@@ -20,3 +20,4 @@ export * from './manager/color.manager/color.model';
 export * from './manager/color.manager/solver.model';
 export * from './manager/animation.manager/animation.manager';
 export * from './type/animation.type';
+export * from './type/animation.function';
